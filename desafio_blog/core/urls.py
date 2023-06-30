01aed5847e95a365c1bee1fe9280b318459448c5
Editty <blancoedith00@gmail.com>
2023-06-30 19:48:41 -0300

    creacion del proyecto desafio 10

---
+++ desafio_blog/core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from core import views
+
+urlpatterns = [
+
+    path('index/', views.indexView, name='index'),
+
+    path('publicaciones/', views.publicacionesView, name ='publicaciones'),
+]
