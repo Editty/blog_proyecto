9a69442de0da2678bd0acb9091bcd155c959f94a
Victor Sanchez <sanbombazos@gmail.com>
2023-07-13 10:46:52 -0300

    clase categorias

--- desafio_blog/publicaciones/forms.py
+++ desafio_blog/publicaciones/forms.py
@@ -1,8 +1,16 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Publicaciones
 
 
-class CrearPublicacionForm(ModelForm):
+class CrearPublicacionForm(forms.ModelForm):
     class Meta:
         model = Publicaciones
-        fields = ['titulo', 'post']
+        fields = ['categoria', 'titulo', 'post']
+        
+        widgets = {
+            # AGREGAMOS LA CLAVE/VALOR DE 'CATEGORIA' EN EL DICCIONARIO WIDGETS
+            'categoria': forms.Select(attrs={'class': 'form-select', 'placeholder':'Categoria'}),
+            'titulo': forms.TextInput(attrs={'placeholder': 'Aca va el titulo', 'class': 'from-control'}),
+            'post': forms.Textarea(attrs={'placeholder': 'Dejanos un Comentario aqui...', 'class': 'from-control'}),
+        }
+
--- desafio_blog/publicaciones/models.py
+++ desafio_blog/publicaciones/models.py
@@ -2,10 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+# Clase que renderiza las categorias:
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    activo = models.BooleanField(default=True)
+    creacion = models.DateTimeField(auto_now_add=True)
+    actualizacion = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        ordering = ['nombre']
+
+#Clase que renderiza las publicaciones
+
 class Publicaciones(models.Model):
     fecha = models.DateField(auto_now_add=True)
     titulo = models.CharField(max_length=255)
     post = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    categoria = models.ForeignKey(Categoria, on_delete= models.SET_NULL, related_name='posteos', null=True)
     
     def __str__(self):
         return self.titulo
