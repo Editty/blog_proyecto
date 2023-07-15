7549cca0777de1e99b4fcc5b17b5837c421a2bd6
Victor Sanchez <sanbombazos@gmail.com>
2023-07-15 14:55:32 -0300

    registrar Usuario

--- desafio_blog/usuarios/views.py
+++ desafio_blog/usuarios/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Usuario
+from .forms import RegistrarseForm
 
 # Create your views here.
+
+class RegistroView(CreateView):
+    model = Usuario
+    template_name = 'usuarios/registro.html'
+    form_class = RegistrarseForm
