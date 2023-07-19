275dc6084a2074fdd242886e1bee77df0b7abc02
Victor Sanchez <sanbombazos@gmail.com>
2023-07-19 12:44:38 -0300

    Comentarios y Mixins

--- desafio_blog/usuarios/views.py
+++ desafio_blog/usuarios/views.py
@@ -1,8 +1,11 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Usuario
 from .forms import RegistrarseForm
 from django.urls import reverse
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -13,3 +16,9 @@ class RegistroView(CreateView):
     
     def get_success_url(self):
         return reverse('publicaciones:publicaciones')
+    
+    def form_valid(self, form):
+        respuesta = super().form_valid(form)
+        usuario = form.save()
+        login(self.request, usuario)
+        return respuesta
