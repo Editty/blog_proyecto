b2ad72027b630ef12e630a18ab89e2fd4417c79a
Francisco E. Zalazar <zalazarfe@gmail.com>
2023-07-21 09:19:54 -0300

    últimos cambios

--- desafio_blog/desafio_blog/settings.py
+++ desafio_blog/desafio_blog/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'prueba_final',
         'USER': 'root',
-        'PASSWORD': '123456789',
+        'PASSWORD': '08P@55w0rD12',
         'HOST': 'localhost',
         'PORT': '3306',
     }
